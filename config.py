56c0913f52254bdb35ff995bc6f22b5f13fb4c4f
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-19 10:17:18 +0300

    added config options

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
+import os
+
 class config:
-    pass
+        
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+        MAIL_SERVER = 'smtp.gmail.com'
+        MAIL_PORT = 587
+        MAIL_USE_TLS = False
+        MAIL_USE_SSL = True
+        MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+        MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+        SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
 
 class ProdConfig(config):
 
@@ -7,10 +18,11 @@ class ProdConfig(config):
 
 class DevConfig(config):
 
-    pass
-
     DEBUG= True
     ENV = 'development'
+    
+
+    
 
 config_options = {
     'production': ProdConfig,
