bf35b05381e0da7a7aa3464093860f985a94ccf0
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-19 10:12:32 +0300

    imported flask from flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import config_options
-from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
 
-bootstrap = Bootstrap()
-
-db = SQLAlchemy()
-
-def create_app (environment):
+def create_app():
     app = Flask(__name__)
-    bootstrap.init_app(app)
-
-    app.config.from_object(config_options[environment])
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-
+    return app
