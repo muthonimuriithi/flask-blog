ca6bff2b046b5e9c7596bd10027c139acf99a1de
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-22 20:36:42 +0300

    modified functionalities, cleared typos and added styling

--- app/main/views.py
+++ app/main/views.py
@@ -1,30 +1,31 @@
-from flask import (render_template, request, redirect, 
-                   url_for, abort)
+from flask import render_template, request, redirect,url_for, abort
 from . import main
 from ..models import User, Comment, Post, Subscribers
 from flask_login import login_required, current_user
-from .forms import (UpdateProfile, PostForm, 
-                    CommentForm, UpdatePostForm)
+from .forms import UpdateProfile, PostForm, CommentForm, UpdatePostForm
 from datetime import datetime
 import bleach
 from .. import db
 from ..requests import get_quote
 from ..email import welcome_message, notification_message
 
-@main.route("/", methods = ["GET", "POST"])
+@main.route("/")
 def index():
     posts = Post.get_all_posts()
     quote = get_quote()
 
-    if request.method == "POST":
-        new_sub = Subscribers(email = request.form.get("subscriber"))
-        db.session.add(new_sub)
-        db.session.commit()
-        welcome_message("Thank you for subscribing to the MMM blog", 
-                        "email/welcome", new_sub.email)
     return render_template("index.html",
                             posts = posts,
                             quote = quote)
+
+    
+    # new_sub = Subscribers(email = request.form.get("subscriber"))
+    # db.session.add(new_sub)
+    # db.session.commit()
+    # welcome_message("Thank you for subscribing to the MMM blog", 
+    #                     "email/welcome", new_sub.email)
+    
+    
 
 @main.route("/post/<int:id>", methods = ["POST", "GET"])
 def post(id):
--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+export SQLALCHEMY_DATABASE_URI ="postgresql+psycopg2://bless:123@localhost/blog"
+export MAIL_USERNAME='loisemuthoni181@gmail.com'
+export MAIL_PASSWORD='0725408650'
+export SECRET_KEY ='JHGVBN'
+
 python3 manage.py server
+# python3 manage.py shell
+
+# python manage.py db init
+# python manage.py db migrate -m "Initial Migration"
+# python manage.py db upgrade
