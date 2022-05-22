21a19e4cf7333829e05235bbb330305ce3856310
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-22 19:47:53 +0300

    configured my squalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@ from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-# from flask_admin import Admin
-from config import config_options
+
+
+from config import config_options, ProdConfig
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -14,31 +15,31 @@ login_manager.login_view = "auth.login"
 photos = UploadSet("photos", IMAGES)
 mail = Mail()
 bootstrap = Bootstrap()
-# admin = Admin()
+
 
 def create_app(config_name):
-    
     app = Flask(__name__)
 
-    # Creating the app configurations
+    
     app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_DATABASE_URI'] = ProdConfig.SQLALCHEMY_DATABASE_URI
 
-    # Initializing flask extensions
+
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
-    # admin.init_app(app)
+   
 
-    # Registering the main app Blueprint
+    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # Registering auth blueprint
+   
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = "/authenticate")
     
-    # Configure UploadSet
+    
     configure_uploads(app, photos)
 
     return app
