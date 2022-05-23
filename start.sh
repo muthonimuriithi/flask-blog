5e308090210a3e697027e3ac4b6d63936cc59f66
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-23 14:54:16 +0300

    modified start.sh file

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export MAIL_PASSWORD='0725408650'
 export SECRET_KEY ='Hgtfvggn'
 
 python3 manage.py server
-# python3 manage.py shell
+
 
 # python manage.py db init
 # python manage.py db migrate -m "Initial Migration"
